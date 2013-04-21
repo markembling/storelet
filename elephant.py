f21778d89f47354e1282926f9b8ffd66c843d27c
Mark Embling <contact@markembling.info>
2013-04-21 18:16:53 +0100

    More bits

--- elephant.py
+++ elephant.py
@@ -1,6 +1,8 @@
 import os
 from tempfile import mkstemp, mkdtemp
 from shutil import rmtree
+from zipfile import ZipFile
+from datetime import datetime
 from boto.s3.connection import S3Connection
 from boto.s3.key import Key
 
@@ -12,15 +14,15 @@ class ZipBackup(object):
     def __enter__(self):
         return self
         
-    def __exit__(self):
+    def __exit__(self, type, value, traceback):
         self.close()
         
     def close(self):
         os.remove(self._path)
     
     def include_directory(self, path, preserve_paths=False, name=None):
-        path = os.abspath(path)
-        with ZipFile(self._path, 'w') as zipfile:
+        path = os.path.abspath(path)
+        with ZipFile(self._path, 'a') as zipfile:
             for base,dirs,files in os.walk(path):
                 for file in files:
                     filename = os.path.join(base, file)
@@ -33,7 +35,7 @@ class ZipBackup(object):
         conn = S3Connection(access_key, secret_key)
         bucket = conn.get_bucket(bucket)
         key = Key(bucket)
-        key.key = '%s_%s.zip' % 
+        key.key = '%s_%s.zip' % \
             (self.name, datetime.now().strftime("%Y%m%d%H%M%S"))
         key.set_contents_from_filename(self._path)
         
@@ -53,14 +55,14 @@ class TemporaryBackupDirectory(object):
         self.path = mkdtemp()
         self._parent = parent
     
-    def __str__():
+    def __str__(self):
         return self.path
     
     def __enter__(self):
         return self
         
-    def __exit__(self):
-        parent.include_directory(self.path, preserve_paths=False, 
-                                 name=self.name)
-        shutil.rmtree(self.path)
+    def __exit__(self, type, value, traceback):
+        self._parent.include_directory(self.path, preserve_paths=False, 
+                                       name=self.name)
+        rmtree(self.path)
 
