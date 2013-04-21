b35fd3fc2b76b4d15de52892bd89e22abd8394ae
Mark Embling <contact@markembling.info>
2013-04-21 19:46:28 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+import os
+from setuptools import setup
+
+def read_file(filename):
+    with open(os.path.join(os.path.dirname(__file__), filename)) as file:
+        return file.read()
+
+setup(name="elephant_backup",
+      version="0.1",
+      description="Simple and easy framework for writing backup scripts",
+      long_description=read_file("README.rst"),
+      author="Mark Embling",
+      author_email="contact@markembling.info",
+      url="http://github.com/markembling/elephant",
+      licence="BSD",
+      py_modules=["elephant_backup"],
+      install_requires=["boto"],
+      classifiers=[
+          "Development Status :: 3 - Alpha",
+          "Intended Audience :: Developers",
+          "Intended Audience :: System Administrators",
+          "Licence :: OSI Approved :: BSD Licence",
+          "Topic :: System :: Archiving :: Backup"
+      ])
